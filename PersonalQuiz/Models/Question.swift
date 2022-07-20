a9d59af4ccfad4c36f048a3c1b0a96e783061745
Alexey Suslov <suslov.aleksey.oo7@gmail.com>
2022-07-21 01:59:51 +0300

    Made Model for Questions and Answers

---
+++ PersonalQuiz/Models/Question.swift
@@ -0,0 +1,53 @@
+//
+//  Question.swift
+//  PersonalQuiz
+//
+//  Created by ALEKSEY SUSLOV on 21.07.2022.
+//
+
+enum ResponseType {
+    case single, multiple, ranged
+}
+
+struct Question {
+    let text: String
+    let type: ResponseType
+    let answers: [Answer]
+}
+
+extension Question {
+    static func getQuestions() -> [Question] {
+        [
+            Question(
+                text: "Какую пищу Вы предпочитаете",
+                type: .single,
+                answers: [
+                    Answer(text: "Стейк", type: .dog),
+                    Answer(text: "Рыба", type: .cat),
+                    Answer(text: "Морковь", type: .rabbit),
+                    Answer(text: "Кукуруза", type: .turtle)
+                ]
+            ),
+            Question(
+                text: "Что Вам нравится больше",
+                type: .multiple,
+                answers: [
+                    Answer(text: "Плавать", type: .dog),
+                    Answer(text: "Спать", type: .cat),
+                    Answer(text: "Обниматься", type: .rabbit),
+                    Answer(text: "Есть", type: .turtle)
+                ]
+            ),
+            Question(
+                text: "Любите ли Вы поездки на машине",
+                type: .ranged,
+                answers: [
+                    Answer(text: "Ненавижу", type: .cat),
+                    Answer(text: "Нервничаю", type: .rabbit),
+                    Answer(text: "Не замечаю", type: .turtle),
+                    Answer(text: "Обожаю", type: .dog)
+                ]
+            )
+        ]
+    }
+}
